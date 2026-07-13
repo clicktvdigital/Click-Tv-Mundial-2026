f880c7bef87dfcc02ae8798d840ca0b3c99f4683
clicktvdigital <clicktvprivado@gmail.com>
2026-07-13 02:04:02 -0500

    Update mundial.js

--- api/mundial.js
+++ api/mundial.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
       "https://api.football-data.org/v4/competitions/WC/matches",
       {
         headers:{
-          "X-Auth-Token": 467c885c07fa49baa40ac78cf636f8b0
+          "X-Auth-Token": TOKEN
         }
       }
     );
