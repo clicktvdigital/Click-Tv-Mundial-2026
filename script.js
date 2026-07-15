929b8c1b9e4f5ff37bc9616826b15ff6dc109941
clicktvdigital <clicktvprivado@gmail.com>
2026-07-14 22:35:39 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -224,7 +224,8 @@ function inicializarRadioLaRed() {
 
     radioPlayer.pause();
     radioPlayer.src = radio.url;
-    radioPlayer.volume = 1;\n    radioPlayer.load();
+    radioPlayer.volume = 1;
+    radioPlayer.load();
 
     try {
       await radioPlayer.play();
